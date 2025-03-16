2fe2bb2ba1541bd7a4b832fef5fe7cd6f55f9fc3
gajulabalakrishna <gajulabalakrishna05@gmail.com>
2025-03-16 12:44:49 +0530

    Delete instance.tf

--- instance.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-00c257e12d6828491"
-  instance_type = "t3.micro"
-  count = 1
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
-}
